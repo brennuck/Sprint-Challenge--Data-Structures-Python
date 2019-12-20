a5aefecc4096773fd0846e3df7fb6e6698b62296
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2019-12-20 12:32:22 -0700

    ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,5 +1,5 @@
 from doubly_linked_list import DoublyLinkedList
-
+# !! go over this when you can think !!
 
 class RingBuffer:
     def __init__(self, capacity):
@@ -8,14 +8,36 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # add elements till capacity is reached
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        elif self.storage.length == self.capacity:
+            to_delete = self.storage.head
+            self.storage.remove_from_head() # removes front of DLL
+            self.storage.add_to_tail(item) # Add new item to back of DLL
+            if to_delete == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        initial_node = self.current
+        list_buffer_contents.append(initial_node.value)
 
+        if initial_node.next is not None:
+            next_node = initial_node.next
+        else:
+            next_node = self.storage.head
+
+        while next_node != initial_node:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
